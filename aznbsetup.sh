2c2066c11c8ab1e582cf56ba3659cc566612e07b
Romain Feron <romain.feron.91@gmail.com>
2019-10-16 11:47:45 +0200

    Test setup Azure Notebooks

---
+++ aznbsetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Setup environment for Azure Notebooks
+# Steps:
+#    - Download, install and setup the latest version of Miniconda
+#    - Create a new conda environment 'snakemake-workshop' with everything required for the workshop
+#    - Activate the environment 'snakemake-workshop'
+
+# Path to miniconda installation
+CONDA_PATH=~/miniconda
+
+# Download the last miniconda installer
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Install conda in silent mode
+sh Miniconda3-latest-Linux-x86_64.sh -b -p "$CONDA_PATH"
+
+# Setup miniconda for current shell and activate base environment
+eval "$("$CONDA_PATH"/bin/conda shell.bash hook)"
+
+# Update conda
+conda update conda -y
+
+# Create workshop environment
+conda env create -f library/workshop.yaml -y
